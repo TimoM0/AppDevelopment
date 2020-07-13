4263a6232093acc67ff5bb6fd79e1a5edcb61236
TimoM0 <68187211+TimoM0@users.noreply.github.com>
2020-07-13 14:44:15 +0200

    Create 01_Datentypen.dart

---
+++ 01_Datentypen.dart
@@ -0,0 +1,14 @@
+main() {
+  int eins = 2;
+  int zwei = 1;
+  print(eins);
+  print(zwei);
+  double komma = 1.1;
+  double punkt = 2.4;
+  print(komma);
+  print(punkt);
+  String hallo = "Tschüss";
+  String tschuess = "Hallo";
+  print(hallo);
+  print(tschuess);
+}
