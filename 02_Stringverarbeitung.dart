dc41dfabd6ec657438d8cb4ad64ce3357c26aff9
TimoM0 <68187211+TimoM0@users.noreply.github.com>
2020-07-14 10:33:17 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,8 @@
+main() {
+  String meinName = "Timo";
+  print("Mein Name ist $meinName");
+  double kommazahl = 1.5;
+  String komma = kommazahl.toString();
+  print(komma);
+  print(meinName.length);
+}
